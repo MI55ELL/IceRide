b0be0745f6fd4b0bd324cc7034ab670b548ceec4
MI55ELL <ellielicius@gmail.com>
2024-05-08 07:07:44 +0100

    assimp and sky box added

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,4 @@
-//------- Ignore this ----------
-#include<filesystem>
-namespace fs = std::filesystem;
-//------------------------------
+
 
 #include"Mesh.h"
 
@@ -28,6 +25,7 @@ GLuint indices[] =
 	0, 2, 3
 };
 
+
 Vertex lightVertices[] =
 { //     COORDINATES     //
 	Vertex{glm::vec3(-0.1f, -0.1f,  0.1f)},
@@ -39,6 +37,42 @@ Vertex lightVertices[] =
 	Vertex{glm::vec3(0.1f,  0.1f, -0.1f)},
 	Vertex{glm::vec3(0.1f,  0.1f,  0.1f)}
 };
+
+float skyBoxVertices[] =
+{
+	//   Coordinates
+	-1.0f, -1.0f,  1.0f,//        7--------6
+	 1.0f, -1.0f,  1.0f,//       /|       /|
+	 1.0f, -1.0f, -1.0f,//      4--------5 |
+	-1.0f, -1.0f, -1.0f,//      | |      | |
+	-1.0f,  1.0f,  1.0f,//      | 3------|-2
+	 1.0f,  1.0f,  1.0f,//      |/       |/
+	 1.0f,  1.0f, -1.0f,//      0--------1
+	-1.0f,  1.0f, -1.0f
+};
+
+unsigned int skyBoxIndices[] =
+{
+	// Right
+	1, 2, 6,
+	6, 5, 1,
+	// Left
+	0, 4, 7,
+	7, 3, 0,
+	// Top
+	4, 5, 6,
+	6, 7, 4,
+	// Bottom
+	0, 3, 2,
+	2, 1, 0,
+	// Back
+	0, 1, 5,
+	5, 4, 0,
+	// Front
+	3, 7, 6,
+	6, 2, 3
+};
+
 
 GLuint lightIndices[] =
 {
@@ -99,12 +133,15 @@ int main()
 
 	// Generates Shader object using shaders default.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
+	Shader skyBoxShader("skyBox.vert", "skyBox.frag");
+	
 	// Store mesh data in vectors for the mesh
 	std::vector <Vertex> verts(vertices, vertices + sizeof(vertices) / sizeof(Vertex));
 	std::vector <GLuint> ind(indices, indices + sizeof(indices) / sizeof(GLuint));
 	std::vector <Texture> tex(textures, textures + sizeof(textures) / sizeof(Texture));
 	// Create floor mesh
 	Mesh floor(verts, ind, tex);
+	
 
 
 	// Shader for light cube
@@ -114,7 +151,7 @@ int main()
 	std::vector <GLuint> lightInd(lightIndices, lightIndices + sizeof(lightIndices) / sizeof(GLuint));
 	// Create light mesh
 	Mesh light(lightVerts, lightInd, tex);
-
+	
 
 
 
@@ -123,12 +160,13 @@ int main()
 	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
 	glm::mat4 lightModel = glm::mat4(1.0f);
 	lightModel = glm::translate(lightModel, lightPos);
+	
 
 	glm::vec3 objectPos = glm::vec3(0.0f, 0.0f, 0.0f);
 	glm::mat4 objectModel = glm::mat4(1.0f);
 	objectModel = glm::translate(objectModel, objectPos);
-
-
+	
+	
 	lightShader.Activate();
 	glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
 	glUniform4f(glGetUniformLocation(lightShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
@@ -136,8 +174,9 @@ int main()
 	glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(objectModel));
 	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
 	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
-
-
+	
+	skyBoxShader.Activate();
+	glUniform1i(glGetUniformLocation(skyBoxShader.ID, "skyBox"), 0);
 
 	
 
@@ -146,6 +185,84 @@ int main()
 
 	// Creates camera object
 	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+
+	unsigned int skyBoxVAO, skyBoxVBO, skyBoxEBO;
+	// passing number of generated vertex arrays and passing where we want it to be stored 
+	// vertex array object used to manage vertex buffer objects and element buffer objects
+	// holds pointers to the EBO and VBO 
+	glGenVertexArrays(1, &skyBoxVAO);
+	// generates buffers to to hold VBO and EBO data in memory
+	// vetex buffer object - holds vertices
+	glGenBuffers(1, &skyBoxVBO);
+	// element buffer object - holds indices
+	glGenBuffers(1, &skyBoxEBO);
+	// setting the VAO array to be the current object puttin it into global context so everything can modify it 
+	glBindVertexArray(skyBoxVAO);
+	// giving the buffer object a type of gl_array_buffer, so it knows how to handel the skyBox vertices
+	glBindBuffer(GL_ARRAY_BUFFER, skyBoxVBO);
+	// binds data of the VBO (vertices) with the VAO so makes the array of the VAO hold the pointer to the vertex buffer object (the indices)
+	// giving the VAO array the amount of the data the pointer is pointing to, reference to the first value in the pointer and telling opengl how to draw it (do not chnge vbertices)
+	glBufferData(GL_ARRAY_BUFFER, sizeof(skyBoxVertices), &skyBoxVertices, GL_STATIC_DRAW);
+	// giving the buffer object a type of element_array_buffer so opengl knows how to handel the skyBox indices
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skyBoxEBO);
+	// giving the VAO array the type of buffer we would like to store reference to, the amount of data that reference is pointing to, the refernce pointer to the first value in EBO and telling opengl not to change indices
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(skyBoxIndices), &skyBoxIndices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	// array of 6 strings holding paths to 6 images 
+	std::string facesCubeMap[6] =
+	{
+		"Textures/rightWall.jpg", // positive x
+		"Textures/leftWall.jpg", // negitive x
+		"Textures/top.jpg", // positive y
+		"Textures/snowGround.jpg", // negitive y
+		"Textures/behindWall.jpg", // positive z
+		"Textures/backWall.jpg" // negitive z
+	};
+
+	//creating a cube map texture 
+	unsigned int cubeMapTexture;
+	glGenTextures(1, &cubeMapTexture);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMapTexture);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+	for (unsigned int i = 0; i < 6; i++) 
+	{
+		int width, height, nrChannels;
+		unsigned char* data = stbi_load(facesCubeMap[i].c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			stbi_set_flip_vertically_on_load(false);
+			glTexImage2D
+			(
+				GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+				0,
+				GL_RGB,
+				width,
+				height,
+				0,
+				GL_RGB,
+				GL_UNSIGNED_BYTE,
+				data
+			);
+			stbi_image_free(data);
+		}
+		else
+		{
+			std::cout << "failed to load texture: " << facesCubeMap[i] << std::endl;
+			stbi_image_free(data);
+		}
+	}
+
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
@@ -161,10 +278,28 @@ int main()
 		// Updates and exports the camera matrix to the Vertex Shader
 		camera.updateMatrix(45.0f, 0.1f, 100.0f);
 
-
+		
 		// Draws different meshes
 		floor.Draw(shaderProgram, camera);
 		light.Draw(lightShader, camera);
+		
+
+		glDepthFunc(GL_LEQUAL);
+		skyBoxShader.Activate();
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 projection = glm::mat4(1.0f);
+		view = glm::mat4(glm::mat3(glm::lookAt(camera.Position, camera.Position + camera.Orientation, camera.Up)));
+		projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
+		glUniformMatrix4fv(glGetUniformLocation(skyBoxShader.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(skyBoxShader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+		glBindVertexArray(skyBoxVAO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMapTexture);
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		glDepthFunc(GL_LESS);
 
 
 		// Swap the back buffer with the front buffer
